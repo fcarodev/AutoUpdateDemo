a230155a93176dcd645ea640a03daca4856741f8
Felipe Caro <fcaro@trust.lat>
2021-07-19 01:40:06 -0400

    initial commit

---
+++ app/src/main/java/com/portfolio/autoupdatedemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.portfolio.autoupdatedemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.enums.Display
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val appUpdater = AppUpdater(this)
+                .setDisplay(Display.DIALOG)
+                .setUpdateFrom(UpdateFrom.JSON)
+        appUpdater.start()
+    }
+}
